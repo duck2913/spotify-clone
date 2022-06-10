17b3bb93470237dd9b0fabb3e74b257602fd791a
duck2913 <minhducdl1010@gmail.com>
2022-06-10 14:54:56 +0700

    library screen

--- src/screens/Library/LibraryScreen.tsx
+++ src/screens/Library/LibraryScreen.tsx
@@ -1,6 +1,43 @@
-
+import { useEffect, useState } from "react";
+import apiClient from "../../api/spotify";
+import PlayCircleIcon from "@mui/icons-material/PlayCircle";
+import "./LibraryScreen.scss";
+import { useNavigate } from "react-router-dom";
 const LibraryScreen = () => {
-	return <div>LibraryScreen</div>;
+	const navigate = useNavigate();
+	const [playlists, setPlaylists] = useState<any>();
+	useEffect(() => {
+		apiClient
+			.get("me/playlists")
+			.then((response) => response.data.items)
+			.then((data) => setPlaylists(data));
+	}, []);
+	function playPlaylist(id: string) {
+		navigate(`/player/${id}`);
+	}
+	return (
+		<div className="library">
+			{playlists?.map((playlist: any) => (
+				<div className="playlist">
+					<img
+						src={playlist.images[0].url}
+						alt="playlist cover"
+						className="playlist__img"
+					/>
+					<div className="playlist__info">
+						<h3 className="playlist__title">{playlist.name}</h3>
+						<p className="playlist__amount">{playlist.tracks.total} tracks</p>
+						<button
+							className="playlist__play"
+							onClick={() => playPlaylist(playlist.id)}
+						>
+							<PlayCircleIcon />
+						</button>
+					</div>
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default LibraryScreen;
